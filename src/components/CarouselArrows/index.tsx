e6c4932904296379ba01bade9626247f2cc86a9e
Paul <itekhi@proton.me>
2026-03-29 06:57:55 +0200

    added className to arrows in CarouselArrows component;

--- src/components/CarouselArrows/index.tsx
+++ src/components/CarouselArrows/index.tsx
@@ -1,6 +1,7 @@
-import React, { ComponentPropsWithRef, useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { EmblaCarouselType } from 'embla-carousel'
 import ArrowButton from '@/components/ArrowButton'
+import { cn } from '@/utilities/ui'
 
 type UsePrevNextButtonsType = {
   prevBtnDisabled: boolean
@@ -47,10 +48,22 @@ export const usePrevNextButtons = (
 
 // type PropType = ComponentPropsWithRef<'button'>
 
-export const PrevButton: React.FC<any> = (props) => {
-  return <ArrowButton className="embla__button embla__button--prev" direction="left" {...props} />
+export const PrevButton: React.FC<any> = ({ className, ...props }) => {
+  return (
+    <ArrowButton
+      className={cn('embla__button embla__button--prev', className)}
+      direction="left"
+      {...props}
+    />
+  )
 }
 
-export const NextButton: React.FC<any> = (props) => {
-  return <ArrowButton className="embla__button embla__button--next" direction="right" {...props} />
+export const NextButton: React.FC<any> = ({ className, ...props }) => {
+  return (
+    <ArrowButton
+      className={cn('embla__button embla__button--next', className)}
+      direction="right"
+      {...props}
+    />
+  )
 }
